38727ba7a4b3729c89e10c88c118186becc8f771
tnssganesh <165705047+tnssganesh@users.noreply.github.com>
2025-10-22 07:09:41 +0530

    Refactor FiltersGroup to use hooks

--- src/components/FiltersGroup/index.js
+++ src/components/FiltersGroup/index.js
@@ -1,91 +1,85 @@
 import {IoMdHome, IoLogoGameControllerB} from 'react-icons/io'
 import {Link} from 'react-router-dom'
-import {Component} from 'react'
+import {useCallback, useContext} from 'react'
 import {RiPlayListAddLine} from 'react-icons/ri'
 import {FaFire} from 'react-icons/fa'
 import LanguageContext from '../../context/LanguageContext'
 import {LightDarkContainer} from './styledComponents'
 import './index.css'
 
-class FiltersGroup extends Component {
-  render() {
-    return (
-      <LanguageContext.Consumer>
-        {value => {
-          const {isDark, changeOption} = value
+const FiltersGroup = () => {
+  const {isDark, changeOption} = useContext(LanguageContext)
 
-          // console.log(isDark)
+  const onClickHome = useCallback(() => {
+    changeOption('Home')
+  }, [changeOption])
 
-          const onClickHome = () => {
-            changeOption('Home')
-          }
-          const onClickTrend = () => {
-            changeOption('Trending')
-          }
-          const onClickGame = () => {
-            changeOption('Gaming')
-          }
-          const onClickSaved = () => {
-            changeOption('Saved Video')
-          }
-          return (
-            <LightDarkContainer
-              outline={isDark}
-              className="filters-group-container"
-            >
-              <ul>
-                <li onClick={onClickHome} className="rating-item">
-                  <Link to="/">
-                    <IoMdHome />
-                    Home
-                  </Link>
-                </li>
+  const onClickTrend = useCallback(() => {
+    changeOption('Trending')
+  }, [changeOption])
 
-                <li onClick={onClickTrend} className="rating-item">
-                  <Link to="/trending">
-                    <FaFire />
-                    Trending
-                  </Link>
-                </li>
-                <li onClick={onClickGame} className="rating-item">
-                  <Link to="/gaming">
-                    <IoLogoGameControllerB /> Gaming
-                  </Link>
-                </li>
-                <li onClick={onClickSaved} className="rating-item">
-                  <Link to="/saved-videos">
-                    <RiPlayListAddLine />
-                    Saved Videos
-                  </Link>
-                </li>
-              </ul>
-              <div className="bootemCard">
-                <p>CONTACT US</p>
-                <div>
-                  <img
-                    className="imglogo"
-                    alt="facebook logo"
-                    src="https://assets.ccbp.in/frontend/react-js/nxt-watch-facebook-logo-img.png"
-                  />
-                  <img
-                    className="imglogo"
-                    alt="twitter logo"
-                    src="https://assets.ccbp.in/frontend/react-js/nxt-watch-twitter-logo-img.png"
-                  />
-                  <img
-                    className="imglogo"
-                    alt="linked in logo"
-                    src="https://assets.ccbp.in/frontend/react-js/nxt-watch-linked-in-logo-img.png "
-                  />
-                </div>
-                <p>Enjoy! Now to see your channels and recommendations!</p>
-              </div>
-            </LightDarkContainer>
-          )
-        }}
-      </LanguageContext.Consumer>
-    )
-  }
+  const onClickGame = useCallback(() => {
+    changeOption('Gaming')
+  }, [changeOption])
+
+  const onClickSaved = useCallback(() => {
+    changeOption('Saved Video')
+  }, [changeOption])
+
+  return (
+    <LightDarkContainer
+      outline={isDark}
+      className="filters-group-container"
+    >
+      <ul>
+        <li onClick={onClickHome} className="rating-item">
+          <Link to="/">
+            <IoMdHome />
+            Home
+          </Link>
+        </li>
+
+        <li onClick={onClickTrend} className="rating-item">
+          <Link to="/trending">
+            <FaFire />
+            Trending
+          </Link>
+        </li>
+        <li onClick={onClickGame} className="rating-item">
+          <Link to="/gaming">
+            <IoLogoGameControllerB /> Gaming
+          </Link>
+        </li>
+        <li onClick={onClickSaved} className="rating-item">
+          <Link to="/saved-videos">
+            <RiPlayListAddLine />
+            Saved Videos
+          </Link>
+        </li>
+      </ul>
+      <div className="bootemCard">
+        <p>CONTACT US</p>
+        <div>
+          <img
+            className="imglogo"
+            alt="facebook logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-facebook-logo-img.png"
+          />
+          <img
+            className="imglogo"
+            alt="twitter logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-twitter-logo-img.png"
+          />
+          <img
+            className="imglogo"
+            alt="linked in logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-linked-in-logo-img.png "
+          />
+        </div>
+        <p>Enjoy! Now to see your channels and recommendations!</p>
+      </div>
+    </LightDarkContainer>
+  )
 }
 
 export default FiltersGroup
